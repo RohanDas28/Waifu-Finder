63cff264019a216bfd505b92dc04b0103f703458
Rohan Das <42172455+RohanDas28@users.noreply.github.com>
2025-03-15 23:26:27 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 const fetchRandomImages = async (options = {}) => {
   try {
     // Example API URL for getting random images
-    const apiUrl = 'https://api.nekosapi.com/v3/images/random';
+    const apiUrl = 'https://api.nekosapi.com/v4/images/random';
 
     // Constructing query parameters based on options
     const queryParams = new URLSearchParams(options);
